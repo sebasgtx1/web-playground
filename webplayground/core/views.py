eebcb5cba117189ea94686967f72290dd91b5b3a
SebasGTX1 <sebas-0202@hotmail.es>
2023-08-24 09:59:44 -0500

    App pages

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "My webplayground"
+        return context
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'tittle' : 'My Web Playgorund' })
 
-def sample(request):
-    return render(request, "core/sample.html")
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
+    #return render(request, "core/sample.html")
