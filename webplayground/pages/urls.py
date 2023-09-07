90d88ecae23eb9b2824736f595c55eee6c62c699
SebasGTX1 <sebas-0202@hotmail.es>
2023-09-07 13:17:00 -0500

    CRUD for pages

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import PageListView, PageDetailView
+from .views import PageListView, PageDetailView, PageCreateView, PageUpdateView, PageDeleteView
 
-urlpatterns = [
+pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
-]
+    path('create/', PageCreateView.as_view(), name='create'),
+    path('update/<int:pk>', PageUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', PageDeleteView.as_view(), name='delete'),
+], 'pages')
