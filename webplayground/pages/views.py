ede66bf43e07e44a5699e03df66efe78256fbff9
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-13 14:31:29 -0500

    Dispatch for secure views with mixins

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,10 +1,21 @@
+from typing import Any
+from django import http
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
 
+class StaffRequiredMixin(object):
+    """
+    Implies that the user is part of the staf
+    """
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 # Create your views here.
 class PageListView(ListView):
     model = Page
@@ -12,22 +23,22 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreateView(CreateView):
+class PageCreateView(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     #fields = ['title', 'content', 'order']
     
     def get_success_url(self):
         return reverse_lazy('pages:pages')
-
-class PageUpdateView(UpdateView):
+    
+class PageUpdateView(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = "_update_form"
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
-class PageDeleteView(DeleteView):
+class PageDeleteView(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
 
