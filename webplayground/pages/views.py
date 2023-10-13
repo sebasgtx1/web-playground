2da23a7ef22b8b40603763ff1cec5dbc7dec2288
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-13 14:40:15 -0500

    implementing decorators insted of mixins

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -4,6 +4,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
@@ -12,9 +14,10 @@ class StaffRequiredMixin(object):
     """
     Implies that the user is part of the staf
     """
+    @method_decorator(staff_member_required)
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return redirect(reverse_lazy('admin:login'))
+        """ if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login')) """
         return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 # Create your views here.
 class PageListView(ListView):
@@ -23,22 +26,23 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreateView(StaffRequiredMixin, CreateView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageCreateView(CreateView):
     model = Page
     form_class = PageForm
-    #fields = ['title', 'content', 'order']
-    
     def get_success_url(self):
         return reverse_lazy('pages:pages')
     
-class PageUpdateView(StaffRequiredMixin, UpdateView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageUpdateView(UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = "_update_form"
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
-class PageDeleteView(StaffRequiredMixin, DeleteView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageDeleteView(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
 
