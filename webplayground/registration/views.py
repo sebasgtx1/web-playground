e8f9f756e4b26c4ba376fcc7c34286f8fbe584a4
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-16 18:50:01 -0500

    Basic registration finished

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms.models import BaseModelForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django import forms
 
 # Create your views here.
 class SignUpView(CreateView):
@@ -10,3 +12,11 @@ class SignUpView(CreateView):
 
     def get_success_url(self) -> str:
         return reverse_lazy('login') + '?register'
+    
+    def get_form(self, form_class=None):
+        form = super(SignUpView, self).get_form()
+        #Real time modification
+        form.fields['username'].widget = forms.TimeInput(attrs={'class':'form-control mb-2', 'placeholder': 'Username'})
+        form.fields['password1'].widget = forms.TimeInput(attrs={'class':'form-control mb-2', 'placeholder': 'Password'})
+        form.fields['password2'].widget = forms.TimeInput(attrs={'class':'form-control mb-2', 'placeholder': 'Repeat Password'})
+        return form
