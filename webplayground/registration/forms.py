a37bf18db98ef1f0bbd294ad4cda5c5b8b91fe61
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-17 00:24:51 -0500

    Update profile Form witn styles

--- webplayground/registration/forms.py
+++ webplayground/registration/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserCreationFormWithEmail(UserCreationForm):
     email = forms.EmailField(required=True, help_text='Required. 254 characters maximum and must be valid')
@@ -14,3 +15,13 @@ class UserCreationFormWithEmail(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError('There is already an account associated with this email')
         return email
+    
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('avatar', 'bio', 'link')
+        widgets = {
+            'avatar': forms.ClearableFileInput(attrs={'class': 'form-control-file mt-3'}),
+            'bio' : forms.Textarea(attrs={'class': 'form-control mt-3', 'rows' : 3, 'placeholder': 'Biography'}),
+            'link' : forms.URLInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Link'}),
+        }
