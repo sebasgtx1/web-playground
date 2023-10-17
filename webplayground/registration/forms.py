b1a6217340fafc26dacaab9daf90a96669656da7
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-17 00:37:34 -0500

    Email Update Form

--- webplayground/registration/forms.py
+++ webplayground/registration/forms.py
@@ -8,7 +8,7 @@ class UserCreationFormWithEmail(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ['username', 'email', 'password1', 'password2']
     
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -19,9 +19,23 @@ class UserCreationFormWithEmail(UserCreationForm):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('avatar', 'bio', 'link')
+        fields = ['avatar', 'bio', 'link']
         widgets = {
             'avatar': forms.ClearableFileInput(attrs={'class': 'form-control-file mt-3'}),
             'bio' : forms.Textarea(attrs={'class': 'form-control mt-3', 'rows' : 3, 'placeholder': 'Biography'}),
             'link' : forms.URLInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Link'}),
         }
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text='Required. 254 characters maximum and must be valid')
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('There is already an account associated with this email')
+        return email
--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.db import models
-from .forms import UserCreationFormWithEmail, ProfileForm
+from django.forms.models import BaseModelForm
+from .forms import UserCreationFormWithEmail, ProfileForm, EmailForm
 from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
@@ -36,3 +37,18 @@ class ProfileUpdate(UpdateView):
     def get_object(self):
         profile, created = Profile.objects.get_or_create(user=self.request.user)
         return profile
+
+@method_decorator(login_required, name='dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_email_form.html'
+
+    def get_object(self):
+        return self.request.user
+    
+    def get_form(self, form_class=None):
+        form = super(EmailUpdate, self).get_form()
+        #Real time modification
+        form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder': 'Email'})
+        return form
