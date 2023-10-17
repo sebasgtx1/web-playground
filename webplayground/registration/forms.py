33b08ba673e96e13462bd8b4742e2c8c3495b583
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-16 19:05:51 -0500

    Fixing input types and adding unique email comprobation

--- webplayground/registration/forms.py
+++ webplayground/registration/forms.py
@@ -8,3 +8,9 @@ class UserCreationFormWithEmail(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('There is already an account associated with this email')
+        return email
--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -15,8 +15,8 @@ class SignUpView(CreateView):
     def get_form(self, form_class=None):
         form = super(SignUpView, self).get_form()
         #Real time modification
-        form.fields['username'].widget = forms.TimeInput(attrs={'class':'form-control mb-2', 'placeholder': 'Username'})
+        form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2', 'placeholder': 'Username'})
         form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder': 'Email'})
-        form.fields['password1'].widget = forms.TimeInput(attrs={'class':'form-control mb-2', 'placeholder': 'Password'})
-        form.fields['password2'].widget = forms.TimeInput(attrs={'class':'form-control mb-2', 'placeholder': 'Repeat Password'})
+        form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder': 'Password'})
+        form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder': 'Repeat Password'})
         return form
