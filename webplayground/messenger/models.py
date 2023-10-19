040289afa4131bf4159db9b91face9652bb0030d
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-18 22:28:01 -0500

    TDD test_add_message_from_user_not_in_thread

--- webplayground/messenger/models.py
+++ webplayground/messenger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
 
 # Create your models here.
 
@@ -13,3 +14,22 @@ class Message(models.Model):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+
+def messages_changed(sender, **kwargs):
+    instance = kwargs.pop('instance', None)
+    action = kwargs.pop('action', None)
+    pk_set = kwargs.pop('pk_set', None)
+    #print(instance, action, pk_set)
+
+    false_pk_set = set()
+    if action == 'pre_add':
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if msg.user not in instance.users.all():
+                false_pk_set.add(msg_pk)
+        
+    # Delete flase_pk_set messages from pk_set
+
+    pk_set.difference_update(false_pk_set)
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
