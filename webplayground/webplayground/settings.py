f48ba8d6e298c577ce8cc2cc7b1330915dcb525e
SebasGTX1 <sebas-0202@hotmail.es>
2023-10-25 16:18:51 -0500

    Debug TRUE

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@z&^ta@*uy!b!3cr8=$i$#%xc8-qs9h6zc=+sa^^$!2hr-c00d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
